38b807facd928881827d1453789b62b5f442a9b0
Joseph <egbenchongjosephayuk@gmail.com>
2025-02-12 09:44:15 +0100

    Add Arcjet rate limiter for queries

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -18,8 +19,8 @@ app.use(cookieParser());
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
-
 app.use(errorMiddleware);
+app.use(arcjetMiddleware);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Subscription-Tracker API!');
